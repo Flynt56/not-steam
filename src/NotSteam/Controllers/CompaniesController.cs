895d7e6f7c68ed5e6bb96b173275aa842c5beafd
Ivan Benko <dbenko18@gmail.com>
2019-07-17 19:44:02 +0200

    Removed excessive 'using' statements from 'ValuesController'. Implemented controller logic for 'CompaniesController'

--- src/NotSteam/Controllers/CompaniesController.cs
+++ src/NotSteam/Controllers/CompaniesController.cs
@@ -1,7 +1,84 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NotSteam.DB;
+using NotSteam.Models;
+
 namespace NotSteam.Controllers
 {
-    public class CompaniesController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompaniesController : ControllerBase
     {
-        
+        private readonly NotSteamContext _context;
+
+        public CompaniesController(NotSteamContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/companies
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Company>>> GetCompanies()
+        {
+            return await _context.Companies.ToListAsync();
+        }
+
+        // GET api/companies/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Company>> GetCompany(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return company;
+        }
+
+        // POST api/companies
+        [HttpPost]
+        public async Task<ActionResult<Company>> PostCompany([FromBody] Company item)
+        {
+            await _context.Companies.AddAsync(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetCompany), new { id = item.Id }, item);
+        }
+
+        // PUT api/companies/5
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Company>> PutCompany(int id, [FromBody] Company item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        // DELETE api/companies/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany(int id)
+        {
+            var company = await _context.Companies.FindAsync(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            _context.Companies.Remove(company);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
