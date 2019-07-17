ecac7cc68030b8b2d05ace946e64a4daa5e02a58
Ivan Benko <dbenko18@gmail.com>
2019-07-17 13:20:22 +0200

    DB context set up

--- src/NotSteam/DB/NotSteamContext.cs
+++ src/NotSteam/DB/NotSteamContext.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using NotSteam.Models;
 
-namespace NotSteam
+namespace NotSteam.DB
 {
     public class NotSteamContext : DbContext
     {
@@ -8,7 +9,13 @@ namespace NotSteam
             : base(options)
         { }
 
-        public DbSet<Blog> Blogs { get; set; }
-        public DbSet<Post> Posts { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Game> Games { get; set; }
+        public DbSet<GameTag> GameTags { get; set; }
+        public DbSet<Library> Libraries { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tag> Tags { get; set; }
     }
 }
