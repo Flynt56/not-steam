4b2c0dd0e62db8ac1dc634f06b3d24cff3aadd60
Ivan Benko <dbenko18@gmail.com>
2019-07-23 10:43:35 +0200

    Started working on model data attributes

--- src/NotSteam/Models/Company.cs
+++ src/NotSteam/Models/Company.cs
@@ -1,13 +1,16 @@
-using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace NotSteam.Models
 {
     public class Company
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "You must provide a Name for your company!")]
         public string Name { get; set; }
+        [Url]
         public string HomepageUri { get; set; }
         public string Description { get; set; }
+        [Url]
         public string LogoImageUri { get; set; }
     }
 }
--- src/NotSteam/Models/Game.cs
+++ src/NotSteam/Models/Game.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace NotSteam.Models
@@ -6,10 +7,14 @@ namespace NotSteam.Models
     public class Game
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "You must provide a Title!")]
         public string Title { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "You must provide a Release Date!")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         [Column(TypeName = "decimal(19,4)")]
+        [Range(0, 99.99)]
         public decimal BasePrice { get; set; }
         public int CompanyId { get; set; }
         public Company Company { get; set; }
