316ad1408452de5c45b205c59ad497e0d01bc77c
Ivan Benko <dbenko18@gmail.com>
2019-07-25 22:39:27 +0200

    Used the Code Cleanup feature to clean up and sort usings. There are no functional changes here.

--- src/NotSteam/Controllers/CompaniesController.cs
+++ src/NotSteam/Controllers/CompaniesController.cs
@@ -1,10 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NotSteam.DB;
 using NotSteam.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace NotSteam.Controllers
 {
