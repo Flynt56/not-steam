c001df9f9d29ec8053d5955ab2b1d7390d9afbbf
Ivan Benko <dbenko18@gmail.com>
2019-07-31 12:28:57 +0200

    Initialize game-list service

--- src/app/game/game-list/game-list.component.ts
+++ src/app/game/game-list/game-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-game-list',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private gameService: GameService
+  ) { }
+
+  private games = [];
 
   ngOnInit() {
+    this.gameService.getAll().subscribe((response: any) => {
+      this.games = response;
+    });
   }
 
 }
---
+++ src/app/game/game.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private readonly GAMES_URL = 'games';
+
+  public getAll() {
+    return this.http.get(environment.apiUrl + this.GAMES_URL);
+  }
+}
