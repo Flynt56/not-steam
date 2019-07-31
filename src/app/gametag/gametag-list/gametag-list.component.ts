21e96d5e0f0197a560eb64d1220905076fe940b5
Ivan Benko <dbenko18@gmail.com>
2019-07-31 20:26:47 +0200

    Add list component files

---
+++ src/app/gametag/gametag-list/gametag-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gametag-list',
+  templateUrl: './gametag-list.component.html',
+  styleUrls: ['./gametag-list.component.scss']
+})
+export class GametagListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
