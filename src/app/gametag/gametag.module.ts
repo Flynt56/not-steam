b5476fd6168b8b94ca69169f9710e751d28aa5e2
Ivan Benko <dbenko18@gmail.com>
2019-07-31 20:22:52 +0200

    Initialize GameTag, Library, Purchase, Review, Tag and User modules

---
+++ src/app/gametag/gametag.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GametagRoutingModule } from './gametag-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GametagRoutingModule
+  ]
+})
+export class GametagModule { }
