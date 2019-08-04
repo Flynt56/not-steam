ff766a7ca7d9e087e765fa52c49511c142485a85
Ivan Benko <dbenko18@gmail.com>
2019-08-04 23:00:28 +0200

    Add navbar component folder

--- src/app/template/navbar/navbar.component.spec.ts
+++ src/app/template/navbar/navbar.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NavbarBaseComponent } from './navbar-base.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('NavbarBaseComponent', () => {
-  let component: NavbarBaseComponent;
-  let fixture: ComponentFixture<NavbarBaseComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NavbarBaseComponent ]
+      declarations: [ NavbarComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(NavbarBaseComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
