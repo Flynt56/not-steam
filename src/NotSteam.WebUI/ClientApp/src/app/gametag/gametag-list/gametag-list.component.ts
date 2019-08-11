2b54bbd5b45941c9d7c41ada2310fa5e8d5b0fca
Ivan Benko <dbenko18@gmail.com>
2019-08-11 18:19:33 +0200

    Implement GameTag list component

--- src/NotSteam.WebUI/ClientApp/src/app/gametag/gametag-list/gametag-list.component.ts
+++ src/NotSteam.WebUI/ClientApp/src/app/gametag/gametag-list/gametag-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GametagService } from '../gametag.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { SpinnerService } from 'src/app/shared/spinner.service';
 
 @Component({
   selector: 'app-gametag-list',
@@ -9,15 +12,40 @@ import { GametagService } from '../gametag.service';
 export class GametagListComponent implements OnInit {
 
   constructor(
-    private gametagService: GametagService
+    private gametagService: GametagService,
+    private toastr: ToastrService,
+    private router: Router,
+    private spinner: SpinnerService
   ) { }
 
   private gametags = [];
 
   ngOnInit() {
+    this.getAllGameTags();
+  }
+
+  getAllGameTags() {
     this.gametagService.getAll().subscribe((response: any) => {
       this.gametags = response;
     });
+  }
+
+  onDelete(gameId, tagId) {
+    if (confirm('Jeste li sigurni?')) {
+      this.gametagService.deleteOne(gameId, tagId).subscribe(result => {
+        this.getAllGameTags();
+        this.toastr.success('Uspješno obrisano!');
+      });
+    }
+  }
+
+  onAdd() {
+    this.router.navigate(['gametags/new']);
+  }
+
+  onEdit(gameId, tagId) {
+    this.spinner.show();
+    this.router.navigate(['gametags', gameId, tagId]);
   }
 
 }
