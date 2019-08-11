1237b095dbf3458f6473f08d9004ac6e9fb82263
Ivan Benko <dbenko18@gmail.com>
2019-08-11 18:04:20 +0200

    Implement tag-form.component

--- src/NotSteam.WebUI/ClientApp/src/app/tag/tag-form/tag-form.component.ts
+++ src/NotSteam.WebUI/ClientApp/src/app/tag/tag-form/tag-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TagService } from '../tag.service';
+import { SpinnerService } from 'src/app/shared/spinner.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-tag-form',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TagFormComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private tagService: TagService,
+    private spinner: SpinnerService,
+    private router: Router,
+    private toastr: ToastrService
+  ) { }
+
+  public tag: any = {};
+  public errorMessage = '';
 
   ngOnInit() {
+    this.route.paramMap.subscribe(params => {
+      const tagId = params.get('id');
+
+      if (tagId != null) {
+        this.getTag(tagId);
+      }
+    });
+  }
+
+  getTag(tagId) {
+    this.tagService.getOne(tagId).subscribe(response => {
+      this.tag = response;
+      this.spinner.hide();
+    });
+  }
+
+  onSubmit() {
+    this.spinner.show();
+
+    this.tagService.submit(this.tag).subscribe(
+      () => {
+        this.toastr.success('Uspješno izvršeno!');
+        this.router.navigate(['tags']);
+        this.spinner.hide();
+      },
+      (response: any) => {
+        const firstError = response.error.errors;
+        const firstKey = Object.keys(firstError)[0];
+        this.errorMessage = firstError[firstKey][0];
+        this.spinner.hide();
+      });
   }
 
 }
