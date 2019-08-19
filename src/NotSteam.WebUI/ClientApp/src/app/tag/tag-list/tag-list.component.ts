cd910385aa240d0aae98b61936017aa0912475e1
Ivan Benko <dbenko18@gmail.com>
2019-08-19 10:53:32 +0200

    Add pagination methods to tag. Add tag service.

--- src/NotSteam.WebUI/ClientApp/src/app/tag/tag-list/tag-list.component.ts
+++ src/NotSteam.WebUI/ClientApp/src/app/tag/tag-list/tag-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { TagService } from '../tag.service';
-import { ToastrService } from 'ngx-toastr';
 import { Router } from '@angular/router';
-import { SpinnerService } from 'src/app/shared/spinner.service';
+import { TagList } from '../model/tag-list';
+import { CommonService } from 'src/app/shared/common.service';
+import { Pagination } from 'src/app/shared/Response/Pagination';
 
 @Component({
   selector: 'app-tag-list',
@@ -13,20 +14,24 @@ export class TagListComponent implements OnInit {
 
   constructor(
     private tagService: TagService,
-    private toastr: ToastrService,
     private router: Router,
-    private spinner: SpinnerService
+    private common: CommonService
   ) { }
 
-  private tags = [];
+  private tags = Array<TagList>();
+  private pagination: Pagination;
 
   ngOnInit() {
     this.getAllTags();
   }
 
   getAllTags() {
-    this.tagService.getAll().subscribe((response: any) => {
-      this.tags = response;
+    this.tagService
+    .getPage(1)
+    .subscribe(({ data, pagination }) => {
+      this.pagination = pagination;
+      this.tags = data;
+      this.common.hide();
     });
   }
 
@@ -34,7 +39,7 @@ export class TagListComponent implements OnInit {
     if (confirm('Jeste li sigurni?')) {
       this.tagService.deleteOne(tagId).subscribe(result => {
         this.getAllTags();
-        this.toastr.success('Uspješno obrisano!');
+        this.common.success('Uspješno obrisano!');
       });
     }
   }
@@ -44,7 +49,7 @@ export class TagListComponent implements OnInit {
   }
 
   onEdit(tagId) {
-    this.spinner.show();
+    this.common.show();
     this.router.navigate(['tags', tagId]);
   }
 
--- src/NotSteam.WebUI/ClientApp/src/app/tag/tag.service.ts
+++ src/NotSteam.WebUI/ClientApp/src/app/tag/tag.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { TagList } from './model/tag-list';
+import { map } from 'rxjs/operators';
+import { PaginationResponse } from '../shared/Response/PaginationResponse';
+import { DetailResponse } from '../shared/Response/DetailResponse';
+import { TagDetails } from './model/tag-details';
 
 @Injectable({
   providedIn: 'root'
@@ -21,12 +26,40 @@ export class TagService {
     return this.getRootUrl() + '/' + tagId;
   }
 
+  private formatPageUrl(page) {
+    return this.getRootUrl() + '?page=' + page;
+  }
+
   public getAll() {
     return this.http.get(this.getRootUrl());
   }
 
-  public getOne(tagId) {
-    return this.http.get(this.formatUrl(tagId));
+  public getPage(page = 1) {
+    return this
+      .http
+      .get(this.formatPageUrl(page))
+      .pipe(
+        map((raw: PaginationResponse<TagList>) => {
+          return raw.response;
+        })
+      );
+  }
+
+  public getDropdown() {
+    return this
+      .http
+      .get(this.getRootUrl() + '/dropdown');
+  }
+
+  public getOne(id) {
+    return this
+      .http
+      .get(this.formatUrl(id))
+      .pipe(
+        map((raw: DetailResponse<TagDetails>) => {
+          return raw.response;
+        })
+      );
   }
 
   public deleteOne(tagId) {
