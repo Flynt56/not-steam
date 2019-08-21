a4230dd3375884fb615aaedfc9a513ab939fc5df
Ivan Benko <dbenko18@gmail.com>
2019-08-21 11:07:10 +0200

    Authorization initial setup

---
+++ src/NotSteam.Api/Controllers/AuthController.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using NotSteam.Infrastructure.DB;
+
+namespace NotSteam.Api.Controllers
+{
+    [Authorize]
+    public abstract class AuthController : BaseController
+    {
+        public AuthController(NotSteamContext context, IMapper mapper) : base(context, mapper)
+        {
+        }
+    }
+}
