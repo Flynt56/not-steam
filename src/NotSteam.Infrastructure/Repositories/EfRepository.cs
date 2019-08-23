24c0dd20e3100508611c2ee9583f62329565d26e
Ivan Benko <dbenko18@gmail.com>
2019-08-23 17:14:35 +0200

    Add EfRepository repository implementation Add SpecificationEvaluator for evaluating specifications in EfRepository

---
+++ src/NotSteam.Infrastructure/Repositories/EfRepository.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NotSteam.Core.Interfaces.Repositories;
+using NotSteam.Core.Interfaces.Specifications;
+using NotSteam.Infrastructure.DB;
+using NotSteam.Infrastructure.Specifications;
+using NotSteam.Model.Interfaces;
+using NotSteam.Model.Models;
+
+namespace NotSteam.Infrastructure.Repositories
+{
+    public class EfRepository<T> : IAsyncRepository<T> where T : EntityBase, IAggregateRoot
+    {
+        protected readonly NotSteamContext _dbContext;
+
+        public EfRepository(NotSteamContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).ToListAsync();
+        }
+
+        public async Task<int> CountAsync(ISpecification<T> spec)
+        {
+            return await ApplySpecification(spec).CountAsync();
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _dbContext.Set<T>().Add(entity);
+            await _dbContext.SaveChangesAsync();
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_dbContext.Set<T>().AsQueryable(), spec);
+        }
+    }
+}
