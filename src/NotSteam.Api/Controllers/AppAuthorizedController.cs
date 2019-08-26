8dd1afc0bd22ac5e2daa6b984448fd8ae062f61f
Ivan Benko <dbenko18@gmail.com>
2019-08-26 10:25:28 +0200

    Add authorize using

--- src/NotSteam.Api/Controllers/AppAuthorizedController.cs
+++ src/NotSteam.Api/Controllers/AppAuthorizedController.cs
@@ -1,4 +1,6 @@
-﻿namespace NotSteam.Api.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace NotSteam.Api.Controllers
 {
     [Authorize]
     public abstract class AppAuthorizedController : AppController
